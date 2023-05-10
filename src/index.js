9b76f57b7a6eaedb9711bc70356188a689653fab
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-10 12:29:22 -0300

    linters fix

--- src/index.js
+++ src/index.js
@@ -3,27 +3,25 @@ import printMe from './print.js';
 import './style.css';
 import Icon from './icon.png';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
   const btn = document.createElement('button');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
-     // Add the image to our existing div.
+  // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
-
-
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
   element.appendChild(btn);
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
