9b329119fffb84463ce4c3cb056a925d477eb615
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-12 17:49:13 -0300

    remove function attached to img

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@ import './style.css';
 import TaskList from './populate.js';
 import Task from './Task';
 
-  const newChore = document.querySelector('.addChore');
-  const newList = new TaskList();
-  newList.render();
+const newChore = document.querySelector('.addChore');
+const newList = new TaskList();
+newList.render();
 
-  newChore.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      const description = newChore.value.trim();
-      if (description !== '') {
-        const task = new Task(description, false, newList.length);
-        newList.addTask(task);
-        newList.render();
-        newChore.value = '';
-      }
+newChore.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const description = newChore.value.trim();
+    if (description !== '') {
+      const task = new Task(description, false, newList.length);
+      newList.addTask(task);
+      newChore.value = '';
     }
-  });
-
-
-
+  }
+});
 
 
 
--- src/populate.js
+++ src/populate.js
@@ -11,7 +11,9 @@ class TaskList extends Array {
     this.sort((a, b) => a.index - b.index);
     list.innerHTML = '';
     for (let i = 0; i < this.length; i += 1) {
+      //create li element
       const newLi = document.createElement('li');
+      //create neccesary items
       const checkboxDescriptionDiv = document.createElement('div');
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
@@ -21,17 +23,30 @@ class TaskList extends Array {
       const deleteIconImg = new Image();
       deleteIconImg.src = deleteIcon;
       deleteIconImg.classList.add('deleteBtnn');
+      deleteIconImg.setAttribute('data-id', i);
       checkboxDescriptionDiv.appendChild(checkbox);
       checkboxDescriptionDiv.appendChild(description);
+      //append the items to the li and provide functionality
       newLi.appendChild(checkboxDescriptionDiv);
       newLi.appendChild(deleteIconImg);
       newLi.appendChild(drag_iconImg);
       list.appendChild(newLi);
+
+      deleteIconImg.addEventListener('click', () => {
+        this.removeTask(i);
+        console.log('removed');
+      });
     }
   }
 
   addTask(task) {
     this.push(task);
+    this.render();
+  }
+
+  removeTask(index) {
+    this.splice(index, 1);
+    this.render();
   }
 }
 
