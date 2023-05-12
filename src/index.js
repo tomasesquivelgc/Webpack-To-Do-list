121761145fda345d4ff2ffd1d4a1126582b8301a
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-12 13:29:34 -0300

    array change into class,
    populate() is now render() method of TaskList class
    addTask method create

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // import _ from 'lodash';
 import './style.css';
-import populate from './populate.js';
+import TaskList from './populate.js';
 
-document.body.appendChild(populate());
+const newChore = document.querySelector('addChore');
+
+const newList = new TaskList;
+newList.render();
+
+newChore.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const description = newChore.value.trim();
+    if (description !== '') {
+      const task = {
+        description,
+        completed: false,
+        index: newList.length,
+      };
+      newList.addTask(task);
+      newList.render();
+      newChore.value = '';
+    }
+  }
+});
--- src/populate.js
+++ src/populate.js
@@ -1,38 +1,47 @@
 import icon from './drag_icon.svg';
 
-const array = [
-  {
-    description: 'Do the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Do the laundry',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Walk the dog',
-    completed: false,
-    index: 2,
-  },
-];
-function populate() {
-  array.sort((a, b) => a.index - b.index);
-  for (let i = 0; i < array.length; i += 1) {
-    const newLi = document.createElement('li');
-    const checkboxDescriptionDiv = document.createElement('div');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    const description = document.createTextNode(array[i].description);
-    const iconImg = new Image();
-    iconImg.src = icon;
-    checkboxDescriptionDiv.appendChild(checkbox);
-    checkboxDescriptionDiv.appendChild(description);
-    newLi.appendChild(checkboxDescriptionDiv);
-    newLi.appendChild(iconImg);
-    list.appendChild(newLi);
+class TaskList extends Array {
+  constructor() {
+    super();
+    this.push(
+      {
+        description: 'Do the dishes',
+        completed: false,
+        index: 0,
+      },
+      {
+        description: 'Do the laundry',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Walk the dog',
+        completed: false,
+        index: 2,
+      }
+    );
+  }
+
+  render() {
+    this.sort((a, b) => a.index - b.index);
+    for (let i = 0; i < this.length; i += 1) {
+      const newLi = document.createElement('li');
+      const checkboxDescriptionDiv = document.createElement('div');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      const description = document.createTextNode(this[i].description);
+      const iconImg = new Image();
+      iconImg.src = icon;
+      checkboxDescriptionDiv.appendChild(checkbox);
+      checkboxDescriptionDiv.appendChild(description);
+      newLi.appendChild(checkboxDescriptionDiv);
+      newLi.appendChild(iconImg);
+      list.appendChild(newLi);
+    }
+  }
+  addTask(task) {
+    this.push(task);
   }
 }
 
-export default populate;
+export default TaskList;
