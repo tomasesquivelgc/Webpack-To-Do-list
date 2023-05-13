06dc3039efb9f3b515bdf8f303f09a62cbc9fd52
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-13 17:09:10 -0300

    linters fix

--- src/delete-completed-tasks.js
+++ src/delete-completed-tasks.js
@@ -1,12 +1,12 @@
 const deleteAllCompleted = (task) => {
-  let newArray = [];
-    for (let i=0; i<task.length; i+=1) {
-      if (task[i].completed === false) {
-        newArray.push(task[i]);
-      }
+  const newArray = [];
+  for (let i = 0; i < task.length; i += 1) {
+    if (task[i].completed === false) {
+      newArray.push(task[i]);
     }
-    task.length = 0;
-    task.push(...newArray);
-}
+  }
+  task.length = 0;
+  task.push(...newArray);
+};
 
 export default deleteAllCompleted;
--- src/populate.js
+++ src/populate.js
@@ -1,7 +1,7 @@
 import dragIcon from './images/drag_icon.svg';
 import deleteIcon from './images/delete.svg';
 import toggleCompleted from './complete-task.js';
-import deleteCompletedTasks from './delete-completed-tasks.js'
+import deleteCompletedTasks from './delete-completed-tasks.js';
 
 const list = document.getElementById('list');
 
@@ -18,30 +18,30 @@ class TaskList extends Array {
       // create li element
       const newLi = document.createElement('li');
       // create neccesary items
-        //checkbox
+      // checkbox
       const checkboxDescriptionDiv = document.createElement('div');
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.classList.add('change');
-      if(this[i].completed === true){
+      if (this[i].completed === true) {
         checkbox.checked = true;
-      };
-        //task description
+      }
+      // task description
       const description = document.createElement('input');
       description.type = 'text';
       description.value = this[i].description;
       description.classList.add('taskDescription');
-        //images
+      // images
       const dragIconImg = new Image();
       dragIconImg.src = dragIcon;
       const deleteIconImg = new Image();
       deleteIconImg.src = deleteIcon;
       deleteIconImg.classList.add('deleteBtnn');
       deleteIconImg.setAttribute('data-id', i);
-        //checkboxDescription attach
+      // checkboxDescription attach
       checkboxDescriptionDiv.appendChild(checkbox);
       checkboxDescriptionDiv.appendChild(description);
-      checkboxDescriptionDiv.classList.add('checkboxDescription')
+      checkboxDescriptionDiv.classList.add('checkboxDescription');
       // append the items to the li and provide functionality
       newLi.appendChild(checkboxDescriptionDiv);
       newLi.appendChild(deleteIconImg);
@@ -58,7 +58,7 @@ class TaskList extends Array {
         this.saveTasksToLocalStorage();
       });
 
-      checkbox.addEventListener('change', () => { 
+      checkbox.addEventListener('change', () => {
         this.toggleCompleted(i);
         this.saveTasksToLocalStorage();
       });
