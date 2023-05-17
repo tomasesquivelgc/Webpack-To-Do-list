e94f5c2f5ab20bc8533bf09591348b1f19b2725c
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-17 17:43:16 -0300

    working correction
    DRY YAGNI and KISS good

--- src/populate.js
+++ src/populate.js
@@ -1,7 +1,5 @@
-import dragIcon from './images/drag_icon.svg';
-import deleteIcon from './images/delete.svg';
-import toggleCompleted from './complete-task.js';
-import deleteAllComplete from './modules/delete-complete-tasks.js';
+import { toggleCompleted, deleteAllComplete } from './task-helpers.js';
+import createListItem from './create-list-item.js';
 
 const list = document.getElementById('list');
 
@@ -14,55 +12,12 @@ class TaskList extends Array {
   render() {
     list.innerHTML = '';
     this.sort((a, b) => a.index - b.index);
-    for (let i = 0; i < this.length; i += 1) {
-      // create li element
-      const newLi = document.createElement('li');
-      // create neccesary items
-      // checkbox
-      const checkboxDescriptionDiv = document.createElement('div');
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.classList.add('change');
-      if (this[i].completed === true) {
-        checkbox.checked = true;
-      }
-      // task description
-      const description = document.createElement('input');
-      description.type = 'text';
-      description.value = this[i].description;
-      description.classList.add('taskDescription');
-      // images
-      const dragIconImg = new Image();
-      dragIconImg.src = dragIcon;
-      const deleteIconImg = new Image();
-      deleteIconImg.src = deleteIcon;
-      deleteIconImg.classList.add('deleteBtnn');
-      deleteIconImg.setAttribute('data-id', i);
-      // checkboxDescription attach
-      checkboxDescriptionDiv.appendChild(checkbox);
-      checkboxDescriptionDiv.appendChild(description);
-      checkboxDescriptionDiv.classList.add('checkboxDescription');
-      // append the items to the li and provide functionality
-      newLi.appendChild(checkboxDescriptionDiv);
-      newLi.appendChild(deleteIconImg);
-      newLi.appendChild(dragIconImg);
+
+    this.forEach((task, i) => {
+      const newLi = createListItem(task, i);
       list.appendChild(newLi);
+    });
 
-      deleteIconImg.addEventListener('click', () => {
-        this.removeTask(i);
-      });
-
-      description.addEventListener('change', (event) => {
-        const newDescription = event.target.value;
-        this[i].description = newDescription;
-        this.saveTasksToLocalStorage();
-      });
-
-      checkbox.addEventListener('change', () => {
-        this.toggleCompleted(i);
-        this.saveTasksToLocalStorage();
-      });
-    }
     this.saveTasksToLocalStorage();
   }
 
@@ -73,9 +28,9 @@ class TaskList extends Array {
 
   removeTask(position) {
     this.splice(position, 1);
-    for (let i = 0; i < this.length; i += 1) {
-      this[i].index = i + 1;
-    }
+    this.forEach((task, i) => {
+      task.index = i + 1;
+    });
     this.render();
   }
 
